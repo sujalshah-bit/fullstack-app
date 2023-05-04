e908405c798238689b402f3d5aedef1535c2d436
Sujal Shah <sujalshah28092004@gmail.com>
2023-05-05 03:40:15 +0530

    basic setup for frontend and navbar styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-
-import './App.css';
-import Nav from './components/Nav';
-
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <Nav/>
+        <Nav />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
     </>
   );
 }
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {FaBars} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 const Nav = () => {
  const [toggle, setToggle] = useState(false)
   return (
@@ -11,11 +12,11 @@ const Nav = () => {
             Navbar
         </div>
         <ul className='navlinks'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Login</li>
-            <li>Registration</li>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+            <li><Link to={'/contact'}>Contact</Link></li>
+            <li><Link to={'/login'}>Login</Link></li>
+            <li><Link to={'/signup'}>Registration</Link></li>
         </ul>
         <div className='bar'>
             <FaBars onClick={()=>setToggle(!toggle)} />
@@ -23,11 +24,11 @@ const Nav = () => {
     </nav>
     <div className={toggle ? 'unhide navlink_mob' : 'hide navlink_mob'}>
         <ul>
-        <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Login</li>
-            <li>Registration</li>
+        <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+            <li><Link to={'/contact'}>Contact</Link></li>
+            <li><Link to={'/login'}>Login</Link></li>
+            <li><Link to={'/signup'}>Registration</Link></li>
         </ul>
     </div>
     </>
