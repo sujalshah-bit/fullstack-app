d50c5fa2bc63409bb63f84c6e2d8446316b85341
Sujal Shah <sujalshah28092004@gmail.com>
2023-05-09 23:23:25 +0530

    storing data of user in react state

--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -1,42 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BsFillPersonFill, BsPersonWorkspace} from 'react-icons/bs'
 import {AiTwotoneMail, AiTwotonePhone} from 'react-icons/ai'
 import {RiLockPasswordFill} from 'react-icons/ri'
 import {GiConfirmed} from 'react-icons/gi'
 const Registration = () => {
+    const [user, setUser] = useState({
+        name:'', email:'', Phone:0, Work:'', Password:'', cPassword:''
+    })
+    let name, value;
+    const handleChange =(e)=>{
+        e.preventDefault()
+        name = e.target.name
+        value = e.target.value
+        setUser({...user, [name]: value })
+    }
+    console.log(user.name);
   return (
     <>
         <section className="registration">
             <div className="box">
-                <form action="">
+                <form action="" >
                     <h1 className='form_head'>SignUp</h1>
                     <div className="sub_box">
                         <label htmlFor="Name"><BsFillPersonFill/></label>
-                        <input type="text" id='Name' placeholder='Your name' autocomplete="off" />
+                        <input onChange={handleChange} type="text" value={user.name} id='Name' name = 'name' placeholder='Your name' autoComplete="off" />
                     </div>
                     <div className="sub_box">
                         <label htmlFor="Email"><AiTwotoneMail /></label>
-                        <input type="email" id='Email' placeholder='Your Email' />
+                        <input onChange={handleChange} type="email" value={user.email} id='Email' name = 'email' placeholder='Your Email' />
                     </div>
                     <div className="sub_box">
                         <label htmlFor="Phone"><AiTwotonePhone /></label>
-                        <input type="number" id='Phone' placeholder='Mobile Number' />
+                        <input onChange={handleChange} type="number" value={user.Phone} id='Phone' name = 'Phone' placeholder='Mobile Number' />
                     </div>
                     <div className="sub_box">
                         <label htmlFor="job"><BsPersonWorkspace /></label>
-                        <input type="text" id='job' placeholder='Your Profession' />
+                        <input onChange={handleChange} type="text" value={user.Work} id='job' name = 'Work' placeholder='Your Profession' />
                     </div>
                     <div className="sub_box">
                         <label htmlFor="Password"><RiLockPasswordFill /></label>
-                        <input type="password" id='Password' placeholder='Password' />
+                        <input onChange={handleChange} type="password" value={user.Password} id='Password' name = 'Password' placeholder='Password' />
                     </div>
                     <div className="sub_box">
                         <label htmlFor="cPassword"><GiConfirmed /></label>
-                        <input type="password" id='cPassword' placeholder='Confirm Password' />
+                        <input onChange={handleChange} type="password" value={user.cPassword} id='cPassword' name = 'cPassword' placeholder='Confirm Password' />
                     </div>
-                    <button type="submit">SignUp</button>
+                    <button type="submit" onClick={handleChange}>SignUp</button>
                 </form>
-
             </div>
         </section>
     </>
