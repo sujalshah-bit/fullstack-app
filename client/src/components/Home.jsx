5639125c5d1796b1b76ac614441a25a0e32f6498
Sujal Shah <sujalshah28092004@gmail.com>
2023-05-09 22:57:59 +0530

    Home page styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <>
-        Home
-    </>
-  )
-}
+    <section className="home_page">
+      <div className="home_box">
+        <p>Welcome</p>
+        <h1>We are The Mern Developers</h1>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
