393c5835e458fbf6683352290b827778ae8d7039
Sujal Shah <sujalshah28092004@gmail.com>
2023-05-10 22:37:57 +0530

    added server code

--- server/models/users.js
+++ server/models/users.js
@@ -1,32 +1,60 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  Phone: {
+    type: Number,
+    required: true,
+  },
+  Work: {
+    type: String,
+    required: true,
+  },
+  Password: {
+    type: String,
+    required: true,
+  },
+  cPassword: {
+    type: String,
+    required: true,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
     },
-    email:{
-        type:String,
-        required:true,
-    },
-    Phone:{
-        type:Number,
-        required:true,
-    },
-    Work:{
-        type:String,
-        required:true,
-    },
-    Passwod:{
-        type:String,
-        required:true,
-    },
-    cPasswod:{
-        type:String,
-        required:true,
-    },
-})
+  ],
+});
 
-const User = mongoose.model('USER', userSchema)
+userSchema.pre("save", async function (next) {
+  if (this.isModified("Password")) {
+    this.Password = await bcrypt.hash(this.Password, 12);
+    this.cPassword = await bcrypt.hash(this.cPassword, 12);
+  }
+  next();
+});
 
-module.exports = User 
+//generate Web Token
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    const token = jwt.sign({__id:this.__id},process.env.SECERET_KEY);
+    this.tokens = this.tokens.concat({token:token})
+    await this.save()
+    return token
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const User = mongoose.model("USER", userSchema);
+module.exports = User;
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,45 +1,91 @@
-const express = require('express')
-const router = express.Router()
-require('../db/conn')
-const User = require('../models/users')
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt  = require("jsonwebtoken")
+require("mongoose");
+require("../db/conn");
+const User = require("../models/users");
+const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("hello from the server");
+});
 
-router.post('/register' ,async(req,res)=>{
-    const  { name, email, Phone, Work, Password, cPassword } = req.body
-
-    if (!name || !email || !Phone || !Work || !Password || !cPassword) {
-            return res.status(422).json({error:"Plz filled properly"})
-    } 
-
-    try {
-        const userExist = await User.findOne({email: email})
-        if(userExist){
-            return res.status(422).json({error:"Email already Exist"})
-        }
-        const user = new User({name, email, Phone, Work, Passwod, cPasswod})
-        await user.save()
-        res.status(201).json({message:"succesfully registered"})
-
-    } catch (error) {
-        console.log(error);
+router.post("/register", async (req, res) => {
+  const { name, email, Phone, Work, Password, cPassword } = req.body;
+  if (!name || !email || !Phone || !Work || !Password || !cPassword) {
+    return res.status(422).json({ error: "Please Filed all the field" });
+  }
+  try {
+    const userExits = await User.findOne({ email: email });
+    if (userExits) {
+      return res
+        .status(422)
+        .json({ error: "User Already Exist with this email id" });
+    } else if (Password != cPassword) {
+      return res
+        .status(422)
+        .json({ error: "password does  not match with confirm password" });
+    } else {
+      const user = new User({ name, email, Phone, Work, Password, cPassword });
+      await user.save();
+      res.status(201).json({ message: "User Registered Successfully" });
     }
-})
+  } catch (error) {
+    console.log(err);
+  }
+});
+//Promises way:
+// router.post("/register", (req, res) => {
+//   const { name, email, Phone, Work, Password, cPassword } = req.body;
+//   if (!name || !email || !Phone || !Work || !Password || !cPassword) {
+//       return res.status(422).json({ error: "Please Filed all the field" });
+//     }
+//   User.findOne({ email: email }).then((userExits) => {
+//     if (userExits) {
+//       return res
+//         .status(422)
+//         .json({ error: "User Already Exist with this email id" });
+//     }
+//     const user = new User({ name, email, Phone, Work, Password, cPassword });
 
-router.post('/login', async (req, res)=>{
-    const  {email, Password} = req.body
-    if(!email || Password){
-        return res.status(422).json({error:"Plz filled properly"})
+//     user
+//       .save()
+//       .then(() => {
+//         res.status(201).json({ message: "User Registered Successfully" });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   });
+// });
+
+//login Route
+router.post("/login", async (req, res) => {
+  const { email, Password } = req.body;
+  try {
+    if (!email || !Password) {
+      return res.status(422).json({ error: "Please Filed all the field" });
     }
-    const userLogin = await User.findOne({email:email})
-
-    if(!userLogin){
-        res.status(422).json({message:"Invalid credentials"})
-    }else{
-        res.status(201).json({message:"successfully login"})
-
+    const emailExist = await User.findOne({ email: email });
+    // console.log(emailExist);
+    if (emailExist) {
+      const isMatch = await bcrypt.compare(Password, emailExist.Password);
+      const token = await emailExist.generateAuthToken()
+      res.cookie("jwtToken",token,{
+        expires:new Date(Date.now() + 25892000000),
+        httpOnly:true
+      })
+      if (!isMatch) {
+        res.status(422).json({ error: "Invalid credentials" });
+      } else {
+        res.status(201).json({ message: "User reg successfully" });
+      }
+    } else {
+      res.status(422).json({ error: "Invalid credentials" });
     }
-})
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
